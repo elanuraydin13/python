202136769bb4dbaf1d2da609a33a9c4e372d348b
elanuraydin13 <elanuraydn31@gmail.com>
2024-12-30 15:39:26 +0300

    Add files via upload

---
+++ 20241227/ornek1.py
@@ -0,0 +1,4 @@
+sumiktari=int(input("SU MİKTARINI GİRİNİZ"))
+birimmiktar=20
+fatura=sumiktari*birimmiktar
+print("FATURANIZ",fatura)
---
+++ 20241227/ornek2.py
@@ -0,0 +1,6 @@
+not1=int(input("1.Notunuzu giriniz"))
+not2=int(input("2.Notunuzu giriniz"))
+performans1=int(input("1.Performansı giriniz"))
+performans2=int(input("2.Performansı giriniz"))
+notortalamasi=(not1+not2+performans1+performans2)/4
+print("Notunuz",notortalamasi)
